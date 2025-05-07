2f6bd0d3940347092692519f2d9352608964504c
Robin Loh <robin_loh@outlook.com>
2025-05-08 02:34:01 +0800

    Implement Transfer API

---
+++ models/transfer_models.go
@@ -0,0 +1,21 @@
+package models
+
+type TransferRequest struct {
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Amount string `json:"amount"`
+}
+
+type Transfer struct {
+	From   string  `json:"from"`
+	To     string  `json:"to"`
+	Amount float64 `json:"amount"`
+}
+
+type TransferResponse struct {
+	From          string  `json:"from"`
+	To            string  `json:"to"`
+	Amount        float64 `json:"amount"`
+	TransactionID string  `json:"transaction_id"`
+	Status        string  `json:"status"`
+}
